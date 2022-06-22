acab1840867957142cef7483bf8f1d7aa1a64fee
tabspear <tabspear@github.com>
2022-06-22 15:37:51 -0700

    Update(Client: Tailwind Custom Colors)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./layouts/*.html",
             "./layouts/*.js"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'red': '#E50914',
+      'dark':"#303030",
+      'bodark': "#222"
+    },
     extend: {},
   },
   plugins: [],
