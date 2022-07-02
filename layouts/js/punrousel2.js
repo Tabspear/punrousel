1b2a9e688bb3c36b3653382e8f6afc93a042b368
tabspear <tabspear@github.com>
2022-07-02 14:43:14 -0700

    Feat(Client):Create files and Setup Punrousel

---
+++ layouts/js/punrousel2.js
@@ -0,0 +1,18 @@
+let bookImg = [
+    {
+        img: "/layouts/img/punroulsel2Img/Demirow.jpg"
+    },
+    {
+        img: "/layouts/img/punroulsel2Img/Eloquent JS.jpg"
+    },
+    {
+        img: "/layouts/img/punroulsel2Img/JS NINJA.jpg"
+    },
+    {
+        img: "/layouts/img/punroulsel2Img/Oriely.jpg"
+    },
+    {
+        img: "/layouts/img/punroulsel2Img/python.webp"
+    },
+]
+
